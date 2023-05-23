59c02e8b2dcf798c6c51ad37564cf8df92b622b4
Anders Meinicke <andersmeinicke@gmail.com>
2023-05-23 14:20:04 +0200

    change commandline

--- FirmClasses/Commandline.py
+++ FirmClasses/Commandline.py
@@ -2,15 +2,23 @@ import csv
 import platform
 from FirmClass import Firm
 from AllFirmsClass import AllFirms
-
+import pandas as pd
+import csv
 firm1 = Firm("Microsoft", [0.2, 0.15, 0.32])
 firm2 = Firm("Apple", [0.03, 0.3, 100])
 firm3 = Firm("Steam", [0.2, 0.05, 0.01])
-
 firmList = []
-firmList.append(firm1)
-firmList.append(firm2)
-firmList.append(firm3)
+##firmList.append(firm1)
+##firmList.append(firm2)
+##firmList.append(firm3)
+with open('FirmClasses/firms.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+        line_count += 1
+        firm1 = Firm(row[0],row[1])
+        firmList.append(firm1)
+    print(f'Processed {line_count} lines.')
 def selectFirm(firmList):
     print("Please select a name from the list:")
     for i, Firm in enumerate(firmList):
